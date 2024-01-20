c65fb17309f8bc2219923d3a49f24419ed5c4c31
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-01-20 13:11:15 +0600

    added community section if posted in community

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -13,7 +14,7 @@ type TThreadCardProps = {
   };
   community: {
     name: string;
-    _id: string;
+    id: string;
     image: string | null;
   };
   createdAt: string;
@@ -106,7 +107,27 @@ const ThreadCard = ({
             </div>
           </div>
         </div>
+
+        {/* Todo : Delete thread */}
+        {/* Todo : show comment logs */}
       </div>
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)} - {community.name} Community
+          </p>
+          <Image
+            src={community.image!}
+            alt={community.name}
+            height={14}
+            width={14}
+            className="ml-1 rounded-full size-[14px] object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
