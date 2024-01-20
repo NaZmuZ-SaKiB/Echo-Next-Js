555bbf0c66fc621a0221105e5da11e864eacc538
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-01-20 15:25:53 +0600

    added timestamps

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -1,38 +1,43 @@
 import { Schema, model, models } from "mongoose";
 
-const userSchema = new Schema({
-  id: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-    required: false,
-  },
-  image: String,
-  bio: String,
-  threads: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Thread",
+const userSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
     },
-  ],
-  onboarded: {
-    type: Boolean,
-    default: false,
-  },
-  communities: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Community",
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-  ],
-});
+    name: {
+      type: String,
+      required: false,
+    },
+    image: String,
+    bio: String,
+    threads: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thread",
+      },
+    ],
+    onboarded: {
+      type: Boolean,
+      default: false,
+    },
+    communities: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Community",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = models.User || model("User", userSchema);
 
