5642267a468ab2954e1c22732097a825c2c33db2
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-01-23 18:25:38 +0600

    added loader for ongoarding page and edit profile page

---
+++ app/(root)/profile/edit/loading.tsx
@@ -0,0 +1,16 @@
+import AccountProfileLoading from "@/components/loaders/AccountProfileLoading";
+
+const EditProfilePageLoading = () => {
+  return (
+    <>
+      <h1 className="head-text">Edit Profile</h1>
+      <p className="mt-3 text-base-regular text-light-2">Make any changes</p>
+
+      <section className="mt-12">
+        <AccountProfileLoading btnTitle="Continue" />
+      </section>
+    </>
+  );
+};
+
+export default EditProfilePageLoading;
---
+++ components/loaders/AccountProfileLoading.tsx
@@ -0,0 +1,40 @@
+import { Skeleton } from "../ui/skeleton";
+
+const AccountProfileLoading = ({ btnTitle }: { btnTitle: string }) => {
+  return (
+    <div className="flex flex-col justify-start gap-10">
+      <div className="flex items-center gap-4">
+        <div className="account-form_image-label">
+          <Skeleton className="rounded-full size-[96px] opacity-10" />
+        </div>
+        <div className="flex-1 text-base-semibold text-gray-200">
+          <div className="account-form_image-input" />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3 w-full">
+        <Skeleton className="opacity-20 h-4 w-16" />
+        <div>
+          <Skeleton className="account-form_input h-10" />
+        </div>
+      </div>
+      <div className="flex flex-col gap-3 w-full">
+        <Skeleton className="opacity-20 h-4 w-16" />
+        <div>
+          <Skeleton className="account-form_input h-10" />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3 w-full">
+        <Skeleton className="opacity-20 h-4 w-10" />
+        <Skeleton className="account-form_input h-56" />
+      </div>
+
+      <Skeleton className="w-full h-9 flex justify-center items-center opacity-15">
+        {btnTitle}
+      </Skeleton>
+    </div>
+  );
+};
+
+export default AccountProfileLoading;
