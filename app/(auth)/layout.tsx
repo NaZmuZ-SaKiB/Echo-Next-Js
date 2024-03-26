5ca16811cefc6fd1e7af64fd56ebf9474d328ec6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-03-26 21:50:38 +0600

    removed clerk provider

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactNode } from "react";
@@ -14,15 +13,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className} bg-dark-1`}>
-          <div className="w-full flex justify-center items-center h-screen">
-            {children}
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className} bg-dark-1`}>
+        <div className="w-full flex justify-center items-center h-screen">
+          {children}
+        </div>
+      </body>
+    </html>
   );
 };
 
