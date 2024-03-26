c0476013c20bf60ef5f41bd1c6095627148440c7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-03-26 21:53:26 +0600

    removed clerk provider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
 
 import "@/app/globals.css";
 
+import AuthContextWrapper from "@/contexts/AuthContext";
 import TopBar from "@/components/layout/TopBar";
 import LeftSideBar from "@/components/layout/LeftSideBar";
 import RightSideBar from "@/components/layout/RightSideBar";
 import BottomBar from "@/components/layout/BottomBar";
+import { currentUser } from "@/database/auth/auth.actions";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,26 +17,28 @@ export const metadata: Metadata = {
   description: "A place to share your thoughts",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await currentUser();
+
   return (
-    <ClerkProvider>
+    <AuthContextWrapper>
       <html lang="en">
         <body className={inter.className}>
           <TopBar />
           <main className="flex">
-            <LeftSideBar />
+            <LeftSideBar userId={user ? `${user._id}` : null} />
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
-            <RightSideBar />
+            <RightSideBar userId={user ? `${user._id}` : null} />
           </main>
           <BottomBar />
         </body>
       </html>
-    </ClerkProvider>
+    </AuthContextWrapper>
   );
 }
