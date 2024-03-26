07889537b4cbe5756fc1486cd827e716b3c2312b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-03-26 21:56:51 +0600

    removed clerk

--- app/(root)/create-echo/page.tsx
+++ app/(root)/create-echo/page.tsx
@@ -1,21 +1,17 @@
-import PostThread from "@/components/forms/PostThread";
-import { fetchUser } from "@/database/user/user.actions";
-import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import PostThread from "@/components/forms/PostThread";
+import { currentUser } from "@/database/auth/auth.actions";
 
 const CreateThreadPage = async () => {
   const user = await currentUser();
-
   if (!user) return null;
 
-  const userInfo = await fetchUser(user.id);
-
-  if (!userInfo?.onboarded) redirect("/onboarding");
+  if (!user?.onboarded) redirect("/onboarding");
 
   return (
     <>
       <h1 className="head-text">Create Echo</h1>
-      <PostThread user_Id={`${userInfo._id}`} />
+      <PostThread user_Id={`${user._id}`} />
     </>
   );
 };
