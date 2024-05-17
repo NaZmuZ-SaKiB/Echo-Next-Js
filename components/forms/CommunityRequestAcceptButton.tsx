099f73d455ba3d9103e51e19ab69d4c8b4a9feb1
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-05-17 23:37:43 +0600

    added Community request accept buttons

---
+++ components/forms/CommunityRequestAcceptButton.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  acceptCommunityJoinRequest,
+  rejectCommunityJoinRequest,
+} from "@/database/community/community.actions";
+import { usePathname } from "next/navigation";
+
+type TProps = {
+  requestId: string;
+};
+
+const CommunityRequestAcceptButton = ({ requestId }: TProps) => {
+  const pathName = usePathname();
+
+  const [acceptLoading, setAcceptLoading] = useState(false);
+  const [rejectLoading, setRejectLoading] = useState(false);
+
+  const handleAccept = async () => {
+    setAcceptLoading(true);
+
+    try {
+      await acceptCommunityJoinRequest(requestId, pathName);
+    } catch (error: any) {
+      console.log(error?.message);
+    } finally {
+      setAcceptLoading(false);
+    }
+  };
+
+  const handleReject = async () => {
+    setRejectLoading(true);
+
+    try {
+      await rejectCommunityJoinRequest(requestId, pathName);
+    } catch (error: any) {
+      console.log(error?.message);
+    } finally {
+      setRejectLoading(false);
+    }
+  };
+  return (
+    <div>
+      <Button
+        className="user-card_btn mr-3 disabled:bg-gray-500"
+        onClick={handleAccept}
+        disabled={acceptLoading}
+      >
+        {acceptLoading ? "Accepting..." : "Accept"}
+      </Button>
+      <Button
+        className="text-[12px] text-light-1 disabled:bg-gray-500"
+        size="sm"
+        variant="destructive"
+        onClick={handleReject}
+        disabled={rejectLoading}
+      >
+        {rejectLoading ? "Rejecting..." : "Reject"}
+      </Button>
+    </div>
+  );
+};
+
+export default CommunityRequestAcceptButton;
