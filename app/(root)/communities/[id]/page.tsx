b53bb2bfef7976705080e6ba34ca9b3575c5ce5d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2024-05-17 23:39:24 +0600

    added request tab

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { communityTabs } from "@/constants";
+import { communityTabs, publicCommunityTabs } from "@/constants";
 import ThreadsTab from "@/components/shared/ThreadsTab";
 import {
   fetchCommunityDetails,
@@ -10,6 +10,7 @@ import {
 } from "@/database/community/community.actions";
 import UserCard from "@/components/cards/UserCard";
 import { currentUser } from "@/database/auth/auth.actions";
+import CommunityRequestsTab from "@/components/shared/CommunityRequestsTab";
 
 const SingleCommunityPage = async ({ params }: { params: { id: string } }) => {
   const user = await currentUser();
@@ -28,6 +29,9 @@ const SingleCommunityPage = async ({ params }: { params: { id: string } }) => {
     (member) => member._id === user._id
   );
 
+  const isCommunityOwner =
+    `${communityDetails.createdBy._id}` === `${user._id}`;
+
   return (
     <section>
       <ProfileHeader
@@ -38,7 +42,7 @@ const SingleCommunityPage = async ({ params }: { params: { id: string } }) => {
         imgUrl={communityDetails.image!}
         bio={communityDetails.bio || ""}
         type="Community"
-        communityOwner={`${communityDetails.createdBy._id}` === `${user._id}`}
+        communityOwner={isCommunityOwner}
         isCommunityMember={isCommunityMember}
         requestSent={communityDetails.requests.includes(`${user._id}`)}
       />
@@ -46,17 +50,24 @@ const SingleCommunityPage = async ({ params }: { params: { id: string } }) => {
       <div className="mt-9 max-sm:mt-5">
         <Tabs defaultValue="echos" className="w-full">
           <TabsList className="tab">
-            {communityTabs.map((tab: any) => (
-              <TabsTrigger key={tab.label} value={tab.value} className="tab ">
-                <Image src={tab.icon} alt={tab.label} height={24} width={24} />
-                <p className="max-sm:hidden">{tab.label}</p>
-                {tab.label === "Echos" && (
-                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 text-tiny-medium text-light-2">
-                    {communityThreadsCount}
-                  </p>
-                )}
-              </TabsTrigger>
-            ))}
+            {(isCommunityOwner ? communityTabs : publicCommunityTabs).map(
+              (tab: any) => (
+                <TabsTrigger key={tab.label} value={tab.value} className="tab ">
+                  <Image
+                    src={tab.icon}
+                    alt={tab.label}
+                    height={24}
+                    width={24}
+                  />
+                  <p className="max-sm:hidden">{tab.label}</p>
+                  {tab.label === "Echos" && (
+                    <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 text-tiny-medium text-light-2">
+                      {communityThreadsCount}
+                    </p>
+                  )}
+                </TabsTrigger>
+              )
+            )}
           </TabsList>
 
           <TabsContent value="echos">
@@ -82,13 +93,11 @@ const SingleCommunityPage = async ({ params }: { params: { id: string } }) => {
             </section>
           </TabsContent>
 
-          <TabsContent value="requests">
-            <ThreadsTab
-              currentUser_Id={`${user._id}`}
-              fetchAccount_Id={`${communityDetails._id}`}
-              accountType="Community"
-            />
-          </TabsContent>
+          {
+            <TabsContent value="requests">
+              <CommunityRequestsTab communityId={`${communityDetails._id}`} />
+            </TabsContent>
+          }
         </Tabs>
       </div>
     </section>
